7f1f2753b8df72f14ac89bc6ccc9055ef7dd4ad0
Laino Santos <lainosantos@gmail.com>
2026-09-15 11:11:07 -0300

    test: make daemon startup lock portable on macOS

--- internal/daemonctl/daemonctl_start_unix_test.go
+++ internal/daemonctl/daemonctl_start_unix_test.go
@@ -19,13 +19,18 @@ func TestConcurrentEnsureRunningStartsOneReadyDaemon(t *testing.T) {
 	releasePath := filepath.Join(root, "release")
 	launcherPath := filepath.Join(root, "launcher")
 	script := "#!/bin/sh\n" +
-		"printf 'started\\n' >> \"$GRAPHIT_DAEMON_TEST_COUNT\"\n" +
-		"exec flock \"$GRAPHIT_DAEMON_TEST_PID\" sh -c 'while [ ! -f \"$GRAPHIT_DAEMON_TEST_RELEASE\" ]; do sleep 0.01; done'\n"
+		"exec \"$GRAPHIT_DAEMON_TEST_HELPER\" -test.run '^TestDaemonctlLauncherHelper$'\n"
 	if err := os.WriteFile(launcherPath, []byte(script), 0o755); err != nil {
+		t.Fatal(err)
+	}
+	testExe, err := os.Executable()
+	if err != nil {
 		t.Fatal(err)
 	}
 	t.Setenv(brand.EnvVar("GLOBAL_DIR"), root)
 	t.Setenv(brand.EnvVar("LAUNCHER_PATH"), launcherPath)
+	t.Setenv("GRAPHIT_DAEMON_TEST_HELPER", testExe)
+	t.Setenv("GRAPHIT_DAEMON_TEST_HELPER_PROCESS", "1")
 	t.Setenv("GRAPHIT_DAEMON_TEST_COUNT", countPath)
 	t.Setenv("GRAPHIT_DAEMON_TEST_PID", pidPath)
 	t.Setenv("GRAPHIT_DAEMON_TEST_RELEASE", releasePath)
@@ -88,5 +93,42 @@ func TestConcurrentEnsureRunningStartsOneReadyDaemon(t *testing.T) {
 	}
 	if fileLocked(pidPath) {
 		t.Fatal("temporary daemon did not release its PID lock")
+	}
+}
+
+func TestDaemonctlLauncherHelper(t *testing.T) {
+	if os.Getenv("GRAPHIT_DAEMON_TEST_HELPER_PROCESS") != "1" {
+		return
+	}
+
+	countFile, err := os.OpenFile(os.Getenv("GRAPHIT_DAEMON_TEST_COUNT"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o600)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := countFile.WriteString("started\n"); err != nil {
+		_ = countFile.Close()
+		t.Fatal(err)
+	}
+	if err := countFile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	pidFile, err := os.OpenFile(os.Getenv("GRAPHIT_DAEMON_TEST_PID"), os.O_CREATE|os.O_RDWR, 0o600)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pidFile.Close()
+	if err := flockExclusiveBlocking(pidFile); err != nil {
+		t.Fatal(err)
+	}
+	defer flockProbeRelease(pidFile)
+
+	for {
+		if _, err := os.Stat(os.Getenv("GRAPHIT_DAEMON_TEST_RELEASE")); err == nil {
+			return
+		} else if !os.IsNotExist(err) {
+			t.Fatal(err)
+		}
+		time.Sleep(10 * time.Millisecond)
 	}
 }
